b902cd52de393a38e6c2482532a9ea2757366c85
Joel Torstensson <oed3933@gmail.com>
2020-02-28 12:55:47 +0100

    Add stuff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { useEffect } from 'react';
 
 import Authereum from "authereum";
-import Web3 from "web3";
-import { createLink, validateLink } from "3id-blockchain-utils";
+import Box from "3box";
+//import { createLink, validateLink } from "3id-blockchain-utils";
 
 function App() {
   useEffect(() => {
     const fn = async () => {
       const authereum = new Authereum("kovan");
       const provider = authereum.getProvider();
-      const web3 = new Web3(provider);
-      await web3.currentProvider.enable();
+      window.auth = authereum
+      window.provider = provider
+      console.log('isA', provider.isAuthereum)
 
-      const did = 'did:3:bafypwg9834gf';
-      const account = (await web3.eth.getAccounts())[0];
-      const proof = await createLink(did, account, web3.currentProvider);
-      console.log(proof);
+      const address = (await provider.enable())[0];
 
-      const verified = await validateLink(proof);
-      if (verified) {
-        console.log('Proof is valid', proof);
-      } else {
-        console.log('Proof is invalid');
-      }
+      const box = await Box.openBox(address, provider)
+      console.log(box.DID)
+      const space = await box.openSpace('demo')
+      await space.syncDone
+
+      console.log('linked:', await box.isAddressLinked())
+      console.log('links:', await box.listAddressLinks())
+
+      //await space.public.set('some-message', 'hello world')
+      console.log('content:', await space.public.get('some-message'))
+      window.box = box
+
+
+      //const did = 'did:3:bafyreiay67567jq5ig4ieu3fqfxfpob5ah3av6wayeik5462earutlehki';
+      //console.log('acc', account)
+      //const proof = await createLink(did, account, provider);
+      //console.log(JSON.stringify(proof));
+
+      //const verified = await validateLink(proof);
+      //if (verified) {
+        //console.log('Proof is valid', proof);
+      //} else {
+        //console.log('Proof is invalid');
+      //}
     }
 
     fn();
